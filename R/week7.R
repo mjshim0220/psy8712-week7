702559bfc68a7cc48bc17948478d3c8f247a983c
MJ Shim <shim0220@umn.edu>
2024-03-11 06:57:19 -0500

    fig2

--- R/week7.R
+++ R/week7.R
@@ -23,11 +23,17 @@ week7_tbl %>%
   select(q1:q10) %>% 
   ggpairs()
 
-
 (week7_tbl %>% 
   ggplot(aes(x=timeStart, y=q1))+
   geom_point()+
   labs(x="Date of Experiment", y="Q1 Score")) %>% 
   ggsave(filename="../figs/fig1.png", width=6, height=3)
+
+(week7_tbl %>% 
+    ggplot(aes(x=q1, y=q2, color=gender))+
+    geom_jitter()+
+    labs(color="Participant Gender")) %>% 
+  ggsave(filename="../figs/fig2.png", width=6, height=3)
+
 
 
