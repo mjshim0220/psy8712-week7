85d5c45a11ad389e6124e052f8f9bd27cc60849e
MJ Shim <shim0220@umn.edu>
2024-03-11 07:11:40 -0500

    Fig 4

--- R/week7.R
+++ R/week7.R
@@ -13,8 +13,8 @@ week7_tbl<-read.csv("../data/week3.csv") %>%
          gender=factor(gender, levels=c("M","F"), labels=c("Male", "Female"))) %>% 
   filter(q6==1) %>%
   select(-q6) %>% 
-  mutate(timeSpent=difftime(timeEnd, timeStart, unit="min"))
-
+  mutate(timeSpent=difftime(timeEnd, timeStart, unit="min")) %>% 
+  mutate(timeSpent=as.numeric(timeSpent))
 
 
 
@@ -23,17 +23,25 @@ week7_tbl %>%
   select(q1:q10) %>% 
   ggpairs()
 
-(week7_tbl %>% 
-  ggplot(aes(x=timeStart, y=q1))+
+(ggplot(week7_tbl, aes(x=timeStart, y=q1))+
   geom_point()+
   labs(x="Date of Experiment", y="Q1 Score")) %>% 
   ggsave(filename="../figs/fig1.png", width=6, height=3)
 
-(week7_tbl %>% 
-    ggplot(aes(x=q1, y=q2, color=gender))+
+(ggplot(week7_tbl,aes(x=q1, y=q2, color=gender))+
     geom_jitter()+
     labs(color="Participant Gender")) %>% 
   ggsave(filename="../figs/fig2.png", width=6, height=3)
 
+(ggplot(week7_tbl,aes(x=q1, y=q2))+
+    geom_jitter()+
+    facet_grid(~gender)+
+    labs(color="Participant Gender", x="Score on Q1", y="Score on Q2")) %>%
+  ggsave(filename="../figs/fig3.png", width=8, height=4)
 
+(ggplot(week7_tbl, aes(x=gender, y=timeSpent))+
+    geom_boxplot()+
+    labs(x="Gender", y="Time Elapsed (mins)")) %>% 
+  ggsave(filename="../figs/fig4.png", width=8, height=4)
 
+  
